be497fb43dd681b39b04db8e40ef3c0adaee3a35
이윤서 <ddal0524@inu.ac.kr>
2024-11-12 11:17:59 +0900

    [level 2] Title: 카펫, Time: 0.09 ms, Memory: 66.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int[] solution(int brown, int yellow) {
+        int[] answer = new int[2];
+ 
+        int carpet = brown + yellow;
+ 
+        // yellow가 존재하기 위해서는 가로와 세로의 길이가 3이상이여야 한다.
+        for (int i = 3; i <= carpet; i++) {
+            int col = i; // 세로
+            int row = carpet / col; // 가로
+ 
+            // 가로의 갯수가 3 이하라면 다음 인덱스
+            if (row < 3) {
+                continue;
+            }
+ 
+            // "가로는 세로의 길이보다 크거나 같다" 조건
+            if (row >= col) {
+                if ((row - 2) * (col - 2) == yellow) {
+                    answer[0] = row;
+                    answer[1] = col;
+                    break;
+                }
+            }
+        }
+        return answer;
+    }
+}
